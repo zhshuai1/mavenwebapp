a99a9b6ddfa93f976e997ae71b3b658ef46a83b3
zhshuai1 <zh_ang_ok@yeah.net>
2015-08-22 11:23:35 +0800

    my first spring mvc web app with maven

---
+++ src/main/java/com/zs/controllers/MainController.java
@@ -0,0 +1,26 @@
+package com.zs.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	@RequestMapping(value = "say", method = RequestMethod.GET)
+	public String sayHello(@RequestParam("name") String id, Model model) {
+		System.out.println(id);
+		model.addAttribute("url", "world");
+		return "test";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Map<String, String> model) {
+		System.out.println("hello");
+		model.put("url", "456");
+		return "index";
+	}
+}
